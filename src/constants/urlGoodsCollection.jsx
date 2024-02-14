d55e841d8140623306d2050c202e18b26d3927e1
Anna Matveeva <107624838+Matveeva-Ann@users.noreply.github.com>
2024-02-14 18:43:03 +0200

    backend simulation

--- src/constants/urlGoodsCollection.jsx
+++ src/constants/urlGoodsCollection.jsx
@@ -1,5 +1,5 @@
 
-export const urlGoods = '/etc/secrets/goods/goods.json';
+export const urlGoods = 'https://clothing-store-0xvh.onrender.com/etc/secrets/goods/goods.json';
 export const urlGoodsEn = '/';
 export const urlCategories = '/categories.json';
 export const urlSendOrder = '/';
